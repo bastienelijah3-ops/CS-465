f41ab2e3eab3b924a6f8906d2f8ef352697ddd46
bastienelijah3 <bastienelijah3@gmail.com>
2026-07-26 13:39:49 -0400

    Migrated travel view to render dynamic JSON data using HBS directives

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,4 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+// Read and parse the trips JSON file
+const tripsData = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../data/trips.json'), 'utf8')
+);
+
 /* GET Travel View */
 exports.travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways - Travel' });
+    res.render('travel', { 
+        title: 'Travlr Getaways - Travel',
+        trips: tripsData 
+    });
 };
